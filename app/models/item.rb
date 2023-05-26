ee3b963d2c192f70bd34344eb3e32d86089a5f8e
2316hako <zp5knmepsu@outlook.jp>
2023-05-26 17:39:38 +0900

    商品出品機能1

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :condition_id, presence: true
+  validates :cost_id, presence: true
+  validates :area_id, presence: true
+  validates :day_id, presence: true
+  validates :category_id, presence: true
+  validates :user, presence: true
 end
--- db/migrate/20230526054721_create_items.rb
+++ db/migrate/20230526054721_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string     :name
+      t.text       :description
+      t.integer    :price
+      t.integer    :condition_id
+      t.integer    :cost_id
+      t.integer    :area_id
+      t.integer    :day_id
+      t.integer    :category_id
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
