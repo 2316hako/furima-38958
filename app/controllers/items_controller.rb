3b267f5a5daf77b7238b8a6f9427b234b56046af
2316hako <zp5knmepsu@outlook.jp>
2023-05-29 15:14:48 +0900

    商品出品機能4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,19 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.order("created_at DESC")
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
@@ -10,4 +21,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image).merge(user_id: current_user.id)
   end
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :price, :condition_id, :cost_id, :area_id, :day_id, :category_id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,19 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :cost
+  belongs_to :area
+  belongs_to :day
 
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
-  validates :condition_id, presence: true
-  validates :cost_id, presence: true
-  validates :area_id, presence: true
-  validates :day_id, presence: true
-  validates :category_id, presence: true
+  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :cost_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :area_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :user, presence: true
 end
