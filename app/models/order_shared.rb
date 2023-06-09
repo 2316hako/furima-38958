dc59368c4d20dbc1682f321e288bcf2ae5a6a6f9
2316hako <zp5knmepsu@outlook.jp>
2023-06-09 18:21:22 +0900

    商品購入機能7

--- app/models/order_shared.rb
+++ app/models/order_shared.rb
@@ -14,6 +14,6 @@ class OrderShared
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
-    Shared.create(post_code: post_code, prefecture_id: prefecture_id, city: city, building_name: building_name, telephone_number: telephone_number, order_id: order.id)
+    Shared.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, telephone_number: telephone_number, order_id: order.id)
   end
 end
