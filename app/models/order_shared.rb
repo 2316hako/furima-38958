d80d7952f743ef8619bb9f765114439c00610bdd
2316hako <zp5knmepsu@outlook.jp>
2023-06-13 21:15:14 +0900

    商品購入機能14

--- app/models/order_shared.rb
+++ app/models/order_shared.rb
@@ -6,7 +6,7 @@ class OrderShared
     validates :item_id
     validates :user_id
     validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
-    validates :prefecture_id
+    validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :city
     validates :address
     validates :telephone_number, format: { with: /\A\d{10,11}\z/ }
--- spec/models/order_shared_spec.rb
+++ spec/models/order_shared_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe OrderShared, type: :model do
   before do
     @order_shared = FactoryBot.build(:order_shared)
     user = FactoryBot.create(:user)
-    item = FactoryBot. create(:item)
+    item = FactoryBot.create(:item)
     @order_shared = FactoryBot.build(:order_shared, user_id: user.id, item_id: item.id)
   end
 
@@ -20,6 +20,18 @@ RSpec.describe OrderShared, type: :model do
         @order_shared.post_code = ''
         @order_shared.valid?
         expect(@order_shared.errors.full_messages).to include("Post code can't be blank")
+      end
+
+      it 'prefecture_idが空では購入できない' do
+        @order_shared.prefecture_id = 1
+        @order_shared.valid?
+        expect(@order_shared.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it 'cityが空では購入できない' do
+        @order_shared.city = ''
+        @order_shared.valid?
+        expect(@order_shared.errors.full_messages).to include("City can't be blank")
       end
     end
   end
