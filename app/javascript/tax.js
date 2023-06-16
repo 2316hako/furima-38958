bd531db07fbd3da126b745d404e1444f7e2dc91f
2316hako <zp5knmepsu@outlook.jp>
2023-06-16 15:16:33 +0900

    販売手数料切り捨て

--- app/javascript/tax.js
+++ app/javascript/tax.js
@@ -1,10 +1,10 @@
 function tax (){
   const itemPrice  = document.getElementById("item-price");
   itemPrice.addEventListener("input", () => {
-    const fee = itemPrice.value * 0.1;
+    const fee = Math.floor(itemPrice.value * 0.1);
     const addTaxPrice = document.getElementById("add-tax-price");
     addTaxPrice.innerHTML = `${Math.floor(fee)}`;
-    const SalesProfit = itemPrice.value - fee ;
+    const SalesProfit = Math.floor(itemPrice.value - fee) ;
     const profit = document.getElementById("profit");
     profit.innerHTML = `${Math.floor(SalesProfit)}`;
   });
